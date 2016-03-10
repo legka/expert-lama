948a50c8316bc467d66a5255b33b1bd3b8c98d7b
Oleg <legka.legka@gmail.com>
2016-03-10 00:47:03 -0800

    Maven and Gradle build configs. Checkstyle configuration.

---
+++ src/main/java/legka/expertlama/Main.java
@@ -0,0 +1,9 @@
+package legka.expertlama;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+
+}
---
+++ src/test/java/legka/expertlama/MainTest.java
@@ -0,0 +1,13 @@
+package legka.expertlama;
+
+import org.junit.Test;
+
+public class MainTest {
+    private final Main main = new Main();
+
+    @Test
+    public void test() {
+        main.main(null);
+    }
+
+}
